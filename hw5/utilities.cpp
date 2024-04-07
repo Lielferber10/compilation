271126b8e0728730322b524400dd859504f3e47d
matanohayon <119515197+matanohayon@users.noreply.github.com>
2024-04-07 13:14:20 +0300

    Add files via upload

---
+++ hw5/utilities.cpp
@@ -0,0 +1,17 @@
+#include "utilities.hpp"
+
+using namespace std;
+
+Var::Var()
+{
+    reg = 0;
+}
+
+Var &Var::getInstance(){
+    Var instance;
+    return instance;
+}
+
+string Var::freshVar(){
+    return "%t_" + to_string(reg++);
+}
---
+++ hw5/utilities.hpp
@@ -0,0 +1,15 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <string>
+
+class Var
+{
+    int reg;
+    
+    public:
+        Var();
+        //Var &getInstance();
+        std::string freshVar();
+
+#endif
