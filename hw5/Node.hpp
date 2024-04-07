baefb46e09ce3e932f6f9161d2a6d737ad5bcf19
liel ferber <liel.farber@campus.technion.ac.il>
2024-04-07 11:51:22 +0300

    hw3 files

---
+++ hw5/Node.hpp
@@ -0,0 +1,272 @@
+#ifndef NODE
+#define NODE
+
+#include "hw3_output.hpp"
+#include <vector>
+#include <string>
+#include <iostream>
+
+
+using namespace std;
+using namespace output;
+
+extern int yylineno;
+
+typedef enum {
+    TINT,TBYTE,TBOOL,TVOID,TSTRING,TFUNCTION,TNONE
+}   Types;
+
+class Node {
+    public:
+    int lineno;
+    string text;
+    Types type;
+    
+    Node()
+    {
+        lineno = -1;
+        text = "";
+        type=TNONE; 
+    }
+    Node(int line){
+        lineno=line;
+        text="";
+        type=TNONE; 
+    };
+
+    Node(int line, string txt){
+        lineno=line;
+        text=txt;
+        type=TNONE; 
+    };
+
+    Node(int line, string txt, Types type){
+        lineno=line;
+        text=txt;
+        this->type=type; 
+    };
+
+    bool is_num(){
+        if(type==TINT || type==TBYTE)
+            return true;
+        return false;
+    }
+
+    void printNode()
+    {
+        std::cout << "line: " << lineno << "  text: " << text << "  type: " << type << endl;
+    }
+    //virtual ~Node();
+};
+
+#define YYSTYPE Node*
+
+class Int : public Node {
+    public:
+    Int(int line, string txt) : Node(line,txt)
+    {
+        type=TINT;
+    };
+};
+
+class Byte : public Node {
+    public:
+    Byte(int line, string txt) : Node(line,txt)
+    {
+        type=TBYTE;
+    };
+};
+
+
+class Bool : public Node {
+    public:
+    Bool(int line, string txt) : Node(line,txt)
+    {
+        type=TBOOL;
+    };
+};
+
+class Void : public Node {
+    public:
+    Void(int line, string txt) : Node(line,txt)
+    {
+        type=TVOID;
+    };
+};
+
+class String : public Node {
+    public:
+    String(int line, string txt) : Node(line,txt)
+    {
+        type=TSTRING;
+    };
+};
+
+class Add :public Node {
+    public:
+    Add(Node *a,Node *b)
+    {
+        if ((a->type != TINT)&&(a->type != TBYTE))
+        {
+          errorMismatch(yylineno);
+          exit(0);  
+        }
+
+        if ((b->type != TINT)&&(b->type != TBYTE))
+        {
+          errorMismatch(yylineno);
+          exit(0);  
+        }
+        if(a->type==TINT || b->type == TINT) {
+            type = TINT;
+        } else type= TBYTE;
+        lineno=yylineno;
+        text="";
+    };
+
+    
+};
+
+
+class Sub :public Node {
+    public:
+    Sub(Node *a,Node *b)
+    {
+        if ((a->type != TINT)&&(a->type != TBYTE))
+        {
+          errorMismatch(yylineno);
+          exit(0);  
+        }
+
+        if ((b->type != TINT)&&(b->type != TBYTE))
+        {
+          errorMismatch(yylineno);
+          exit(0);  
+        }
+        if(a->type==TINT || b->type == TINT) {
+            type = TINT;
+        } else type= TBYTE;
+        lineno=yylineno;
+        text="";
+    }; 
+};
+
+class Mul :public Node {
+    public:
+    Mul(Node *a,Node *b)
+    {
+        if ((a->type != TINT)&&(a->type != TBYTE))
+        {
+          errorMismatch(yylineno);
+          exit(0);  
+        }
+
+        if ((b->type != TINT)&&(b->type != TBYTE))
+        {
+          errorMismatch(yylineno);
+          exit(0);  
+        }
+        if(a->type==TINT || b->type == TINT) {
+            type = TINT;
+        } else type= TBYTE;
+        lineno=yylineno;
+        text="";
+    };
+};
+
+class Dev :public Node {
+    public:
+    Dev(Node *a,Node *b)
+    {
+        if ((a->type != TINT)&&(a->type != TBYTE))
+        {
+          errorMismatch(yylineno);
+          exit(0);  
+        }
+
+        if ((b->type != TINT)&&(b->type != TBYTE))
+        {
+          errorMismatch(yylineno);
+          exit(0);  
+        }
+        if(a->type==TINT || b->type == TINT) {
+            type = TINT;
+        } else type= TBYTE;
+        lineno=yylineno;
+        text="";
+    };
+};
+
+
+class Exp : public Node{
+    public:
+    Exp(Node *new_type,Node *exp,int line) //explicit cast
+    {
+        if(!exp->is_num()||!new_type->is_num())
+        {
+            errorMismatch(line);
+            exit(0);
+        }
+        type=new_type->type;
+        lineno=line;
+        text=std::move(exp->text);
+
+    };
+
+    Exp (Node* type, Node* id, Node* exp, int line)
+    {
+        bool valid = false;
+        if(type->type == exp->type) valid = true;
+        else if (type->type == TINT && exp->type == TBYTE) valid = true;
+
+        if (!valid)
+        {
+            errorMismatch(line);
+            exit(0);
+        }
+        this->type=type->type;
+        lineno=line;
+        text = id->text;
+    };
+    
+    Exp (Types type, Node* id, Node* exp, int line)
+    {
+        bool valid=false;
+        if(type==exp->type) valid=true;
+        else if (type == TINT && exp->type == TBYTE) valid=true;
+        
+        if (!valid)
+        {
+            errorMismatch(line);
+            exit(0);
+        }
+        this->type=type;
+        lineno=line;
+        text = id->text;
+    }
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ hw5/scanner.lex
@@ -0,0 +1,58 @@
+%{
+#include "hw3_output.hpp"
+#include "Node.hpp"
+#include <string.h>
+#include "parser.tab.hpp"
+
+%}
+
+%option yylineno
+%option noyywrap
+
+
+highPriorityRelation        [<>]=|[<>]
+lowPriorityRelation         [=!]=
+comment                     \/\/[^\r\n]*[\r|\n|\r\n]?
+identifier                  [a-zA-Z][a-zA-Z0-9]*
+number                      0|[1-9][0-9]*
+string                      \"([^\n\r\"\\]|\\[rnt"\\])+\"
+
+
+
+
+%%
+
+int                             {yylval = new Int(yylineno,yytext); return INT;}
+byte                            {yylval = new Byte(yylineno,yytext); return BYTE;}
+b                               {yylval = new Byte(yylineno,yytext); return B;}
+bool                            {yylval = new Bool(yylineno,yytext); return BOOL;}
+and                             {yylval = new Node(yylineno,yytext); return AND;}
+or                              {yylval = new Node(yylineno,yytext); return OR;}
+not                             {yylval = new Node(yylineno,yytext); return NOT;}
+true                            {yylval = new Bool(yylineno,yytext); return TRUE;}
+false                           {yylval = new Bool(yylineno,yytext); return FALSE;}
+return                          {yylval = new Node(yylineno,yytext); return RETURN;}
+if                              {yylval = new Node(yylineno,yytext); return IF;}
+else                            {yylval = new Node(yylineno,yytext); return ELSE;}
+while                           {yylval = new Node(yylineno,yytext); return WHILE;}
+break                           {yylval = new Node(yylineno,yytext); return BREAK;}
+continue                        {yylval = new Node(yylineno,yytext); return CONTINUE;}
+{identifier}                    {yylval = new Node(yylineno,yytext); return ID;}
+;                               {yylval = new Node(yylineno,yytext); return SC;}
+\(                              {yylval = new Node(yylineno,yytext); return LPAREN;}
+\)                              {yylval = new Node(yylineno,yytext); return RPAREN;}
+\{                              {yylval = new Node(yylineno,yytext); return LBRACE;}
+\}                              {yylval = new Node(yylineno,yytext); return RBRACE;}
+=                               {yylval = new Node(yylineno,yytext); return ASSIGN;}
+{highPriorityRelation}          {yylval = new Node(yylineno,yytext); return HRELOP;}
+{lowPriorityRelation}           {yylval = new Node(yylineno,yytext); return LRELOP;}
+\+                              {yylval = new Node(yylineno,yytext); return PLUS;}
+\-                              {yylval = new Node(yylineno,yytext); return MINUS;}
+\*                              {yylval = new Node(yylineno,yytext); return MULT;}
+\/                              {yylval = new Node(yylineno,yytext); return DEV;}
+{number}                        {yylval = new Int(yylineno,yytext); return NUM;}
+{string}                        {yylval = new String(yylineno,yytext); return STRING;}
+([\r\n\x20\t]|{comment})        {}
+.                               {output::errorLex(yylineno); exit(0);}
+
+%%
